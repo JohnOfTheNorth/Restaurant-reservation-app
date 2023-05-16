5352e33f6febf5d00478d6c3a35f7cbd446583ff
Jonathan <jonrodriguez1396@gmail.com>
2023-05-15 23:38:57 -0400

    commit

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,7 +4,7 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
+
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 const reservationsRouter = require("./reservations/reservations.router");
@@ -13,6 +13,7 @@ const tablesRouter = require("./tables/tables.router");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/reservations", reservationsRouter);
 app.use("/tables", tablesRouter);
